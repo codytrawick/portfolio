3b889595e9eaed28249330eaeca92fe5c85533a3
Cody Trawick <cody.trawick@gmail.com>
2025-11-14 08:32:52 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Container, Typography } from '@mui/material'
+import './App.css'
+
+function App() {
+  return (
+    <Container maxWidth='md'>
+      <Typography variant='h1'>
+        Cody Trawick
+      </Typography>
+    </Container>
+  )
+}
+
+export default App
